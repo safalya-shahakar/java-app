29f058cbff860f110a03cd2b47e2ae00681c431d
safalya-shahakar <safalyashahakar94@gmail.com>
2025-01-16 17:30:57 +0530

    Initial commit: Java application for CI/CD

---
+++ apptest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals("Hello, Jenkins CI/CD Pipeline!", "Hello, Jenkins CI/CD Pipeline!");
+    }
+}
+
